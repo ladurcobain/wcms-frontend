11859e4fa4c0a1573f1e402ffddeb781780b3fde
Ladur Cobain <ladurcobain@Ladurs-MacBook-Pro.local>
2024-02-14 15:51:15 +0700

    up preview youtube

--- resources/views/archive/movie/create.blade.php
+++ resources/views/archive/movie/create.blade.php
@@ -44,6 +44,7 @@
                             <label class="col-sm-3 control-label text-sm-end pt-2">Judul <span class="required">*</span></label>
                             <div class="col-sm-9">
                                 <input type="text" class="form-control" id="title" name="title" placeholder="Judul" autocomplete="off" required />
+                                <span class="help-block">Cara menambahkan <a href="javascript:void(0);" OnClick="modal_youtube();">Url Youtube</a></span>
                             </div>
                         </div>
                         <div class="form-group row pt-3 pb-2">
@@ -70,5 +71,53 @@
         </div>
     </div>
 </div>
+
+<div id="modalYoutube" class="modal animated bounceIn" tabindex="-1" role="dialog" aria-labelledby="modalYoutube" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-body" id="modal-content">
+                <div class="card card-modern">
+                    <div class="card-header">
+                        <h2 class="card-title"></h2>
+                    </div>
+                    <div class="card-body">
+                        <div class="ecommerce-timeline mb-3">
+                            <div class="tm-title">
+                                <h5 class="m-0 pt-2 pb-2 text-dark font-weight-semibold text-uppercase">Cara menambahkan Url Youtube</h5>
+                            </div>
+                            <div class="ecommerce-timeline-items-wrapper">
+                                <div class="ecommerce-timeline-item">
+                                    <small>Buka website <a target="_blank" href="https://www.youtube.com/" class="text-color-primary">Youtube</a></small>
+                                    <p>Kemudian salin <i>url youtube</i> video tersebut</p>
+                                </div>
+                                <div class="ecommerce-timeline-item">
+                                    <p>Mencari video yang ingin bagikan</p>
+                                    <div>
+                                        <ol>
+                                            <li>Buka video yang ingin Anda bagikan</li>
+                                            <li>Di bawah pemutar video, ketuk <i class="bx bx-share"></i> Bagikan</li>
+                                            <li>Akan muncul panel yang menampilkan beberapa opsi berbagi</li>
+                                            <li>Pilih Salin tautan, untuk menyalin dan menempelkan URL video tersebut</li>
+                                        </ol>
+                                    </div>
+                                </div>
+                                <div class="ecommerce-timeline-item">
+                                    <div class="thumbnail-gallery">
+                                        <img class="img-fluid" width="100%" src="{{ asset('assets/img/guide_youtube.png') }}" alt="Pencarian youtube" />
+                                    </div>    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    function modal_youtube() {
+        $('#modalYoutube').modal('show');
+    }
+</script>
 
 @endsection
