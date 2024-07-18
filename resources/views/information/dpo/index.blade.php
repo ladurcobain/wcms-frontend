19248399e8da685505b8d607696c93df740603aa
Ladur Cobain <ladurcobain@Ladurs-MacBook-Pro.local>
2024-07-18 14:43:07 +0700

    del image all

--- resources/views/information/dpo/index.blade.php
+++ resources/views/information/dpo/index.blade.php
@@ -62,7 +62,7 @@
                                 <tr>
                                     <th width="25%">Satuan Kerja</th>
                                     <th>Nama DPO</th>
-                                    <th class="center" width="10%">Gambar</th>
+                                    <!-- <th class="center" width="10%">Gambar</th> -->
                                     <th class="center" width="10%">Status</th>
                                     <th class="center" width="20%">Aksi</th>
                                 </tr>
@@ -73,13 +73,13 @@
                                 <tr>
                                     <td>{{ $row->dpo_satker}}</td>
                                     <td>{{ $row->dpo_name}}</td>
-                                    <td class="center">
+                                    <!-- <td class="center">
                                         <?php if($row->dpo_image != "") { ?>
                                             <img src="{{ $row->dpo_path }}" alt="Webphada" class="img-thumbnail" />
                                         <?php } else { ?>
                                             <img src="{{ asset('assets/img/logo-webphada.png') }}" alt="Webphada" class=" user-image img-thumbnail" />
                                         <?php } ?>   
-                                    </td>
+                                    </td> -->
                                     <td class="center"><span class="badge badge-<?php echo (($row->dpo_status == 1 ? "info" : "dark")); ?>"><?php echo (($row->dpo_status == 1 ? Status::tipeStatus(1) : Status::tipeStatus(2))); ?></span></td>
                                     <td class="center">
                                         <button type="button" class="btn btn-sm btn-warning modalConfirmEdit"
@@ -92,14 +92,14 @@
                                 </tr>
                                 <?php } ?>
                                 <?php } else { ?>
-                                    <tr><td class="center" colspan="5">Data tidak ditemukan</td></tr>
+                                    <tr><td class="center" colspan="4">Data tidak ditemukan</td></tr>
                                 <?php } ?>
                             </tbody>
                             <?php if($results->total() > 0) { ?>
                             <tfoot>    
                                 <tr>
                                     <td>Total <b>{{ $results->total() }}</b> Data</td>
-                                    <td colspan="4"><span style="margin-top: 15px;float:right;">{{ $results->onEachSide(1)->links() }}</span></td>
+                                    <td colspan="3"><span style="margin-top: 15px;float:right;">{{ $results->onEachSide(1)->links() }}</span></td>
                                 </tr>
                             </tfoot>
                             <?php } ?>
